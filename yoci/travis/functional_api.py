8432707d79a390d5944982bf156f9e232b86fe9d
adamlavie <adaml@gigaspaces.com>
2014-09-08 21:49:51 +0300

    logging and bug fix

--- yoci/travis/functional_api.py
+++ yoci/travis/functional_api.py
@@ -4,11 +4,19 @@ import logging
 from yoci.travis.travis import Builds
 from yoci.travis.travis import Job
 
-lgr = logging.getLogger('test logger')
+lgr = logging.getLogger('travis_func_api')
+lgr.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - '
+                              '%(message)s')
+console_handler.setFormatter(formatter)
+lgr.addHandler(console_handler)
 
 
 def _wait_for_build_state(build_id, end):
     builds = Builds(lgr)
+    lgr.info('Waiting for build with ID {0} to reach final state for {1} '
+             'seconds.'.format(build_id, int(end-time.time())))
     while time.time() < end:
         build = builds.show_build(id=build_id)
         state = build['state']
@@ -17,6 +25,8 @@ def _wait_for_build_state(build_id, end):
                 'Build still in progress. Waiting for build process to end')
             time.sleep(10)
         else:
+            lgr.info('Build matching ID {0} has reached a final state: {1}'
+                     .format(build_id, state))
             return build
 
     lgr.warn('Failed waiting for build state to reach passed/failed. Build '
@@ -25,24 +35,29 @@ def _wait_for_build_state(build_id, end):
 
 def _wait_for_commit(repo_name, sha_id, end):
     builds = Builds(lgr, repo=repo_name)
+    lgr.info('Waiting for commit with sha ID {0} and repo {1} for: {2} '
+             'seconds.'.format(sha_id, repo_name, int(end-time.time())))
     while time.time() < end:
         builds_list, commits = builds.list_builds()
         for commit in commits:
             if commit['sha'] == sha_id:
+                lgr.info('Commit matching sha ID {0} was found'.format(sha_id))
                 return commit
 
+        lgr.info('Commit with sha ID \'{0}\' was found for repo {1}'
+                 .format(sha_id, repo_name))
         time.sleep(10)
 
-    lgr.warn('Failed waiting for commit with sha ID {0} on repo {1}'
-             .format(sha_id, repo_name))
+    lgr.warning('Failed waiting for commit with sha ID {0} on repo {1}'
+                .format(sha_id, repo_name))
 
 
 def _get_commit_id(repo_name, sha_id, end, branch_name=None):
     commit = _wait_for_commit(repo_name, sha_id, end)
     if not commit:
         raise RuntimeError(
-            'Failed waiting for commit with sha ID {0} on repo {1} for the'
-            ' duration of {2}ms'.format(sha_id, repo_name, time.time() - end))
+            'Failed waiting for commit with sha ID {0} on repo {1}'
+            .format(sha_id, repo_name))
 
     # For validation reasons. branch not required with sha_id
     if branch_name and branch_name != commit['branch']:
@@ -99,6 +114,7 @@ def get_jobs_status(sha_id, repo_name, branch_name=None, timeout_min=15):
                            .format(timeout_min))
 
     jobs_state = dict()
+    lgr.info('Getting jobs state for build with ID {0}'.format(build_id))
     for job_id in job_ids:
         job = Job(lgr, job_id=job_id).show_job()
         jobs_state.update({job['config']['env']: job['state']})
