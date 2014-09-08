e314913e1e565e023d1e7ebdacbb32d2343794fc
adamlavie <adaml@gigaspaces.com>
2014-09-08 12:10:31 +0300

    added function to retrieve jobs state for a specific commit

---
+++ yoci/travis/functional_api.py
@@ -0,0 +1,107 @@
+import time
+import logging
+
+from yoci.travis.travis import Builds
+from yoci.travis.travis import Job
+
+lgr = logging.getLogger('test logger')
+
+
+def _wait_for_build_state(build_id, end):
+    builds = Builds(lgr)
+    while time.time() < end:
+        build = builds.show_build(id=build_id)
+        state = build['state']
+        if state != 'passed' and state != 'failed':
+            lgr.info(
+                'Build still in progress. Waiting for build process to end')
+            time.sleep(10)
+        else:
+            return build
+
+    lgr.warn('Failed waiting for build state to reach passed/failed. Build '
+             'state is {0}'.format(build['state']))
+
+
+def _wait_for_commit(repo_name, sha_id, end):
+    builds = Builds(lgr, repo=repo_name)
+    while time.time() < end:
+        builds_list, commits = builds.list_builds()
+        for commit in commits:
+            if commit['sha'] == sha_id:
+                return commit
+            else:
+                time.sleep(10)
+
+    lgr.warn('Failed waiting for commit with sha ID {0} on repo {1} for the'
+             ' duration of {2}ms'.format(sha_id, repo_name, time.time() - end))
+
+
+def _get_commit_id(repo_name, sha_id, end, branch_name=None):
+    commit = _wait_for_commit(repo_name, sha_id, end)
+
+    # For validation reasons. branch not required with sha_id
+    if branch_name and branch_name != commit['branch']:
+        raise RuntimeError('Commit with sha_id {0} was made to branch'
+                           ' {1}. Expecting branch {2}'
+                           .format(sha_id, commit['branch'], branch_name))
+    return commit['id']
+
+
+def _get_build_id(builds, commit_id):
+    build_id = None
+    for build in builds:
+        if build['commit_id'] == commit_id:
+            build_id = build['id']
+            break
+    return build_id
+
+
+def get_jobs_status(sha_id, repo_name, branch_name=None, timeout_min=15):
+    '''
+    Returns a dictionary containing job results for the specified commit and
+    repository name
+    :param sha_id:
+                    The unique commit SHA ID.
+    :param repo_name:
+                    The name of the repo the commit was made to.
+    :param branch_name:
+                    The name of the branch the commit was made to
+                    Used mostly for validation.Ignored if not passed.
+    :param timout_min:
+                    The timeout to wait for a build to reach final state.
+                    Default is set to 15 minutes.
+    :return: a dictionary containing job results for the specified commit
+    '''
+    end = end = time.time() + 60 * timeout_min
+
+    commit_id = _get_commit_id(repo_name, sha_id, end, branch_name=branch_name)
+
+    if commit_id is None:
+        raise RuntimeError('Could not find commit matching to sha {0} for repo'
+                           ' {1}'.format(sha_id, repo_name))
+
+    builds = Builds(lgr, repo=repo_name)
+    builds_list, commits = builds.list_builds()
+    build_id = _get_build_id(builds_list, commit_id)
+
+    # We wait for the build to reach final state.
+    build = _wait_for_build_state(build_id, end)
+    if build:
+        job_ids = build['job_ids']
+    else:
+        raise RuntimeError('Failed waiting for build process to finish'
+                           ' for the duration of {0}'
+                           .format(timeout_min))
+
+    jobs_state = dict()
+    for job_id in job_ids:
+        job = Job(lgr, job_id=job_id).show_job()
+        jobs_state.update({job['config']['env']: job['state']})
+    return jobs_state
+
+
+# jobs_state = get_jobs_status('40509789b11d1bf7cb4c785162cb77f9ea927274',
+#                              'cloudify-cosmo/packman',
+#                              branch_name='master',
+#                              timeout_min=15)
---
+++ yoci/travis/travis.py
@@ -0,0 +1,176 @@
+import requests
+import json
+from yoci.utils import import_config
+
+USER_AGENT = 'MyClient/1.0.0'
+ACCEPT = 'application/vnd.travis-ci.2+json'
+LOG_ACCEPT = 'text/plain'
+HEADERS = {'Accept': ACCEPT, 'User-Agent': USER_AGENT}
+LOG_HEADERS = {
+    'Accept': LOG_ACCEPT,
+    'User-Agent': USER_AGENT
+}
+
+DEFAULT_CONFIG_PATH = '/home/adaml/.travis-config.yml'
+
+C = import_config(DEFAULT_CONFIG_PATH)['Travis']
+
+
+def get_token(lgr, auth_url, hs_url, token):
+    lgr.debug('requesting for auth token from: {} with token: {}'.format(
+        auth_url, token))
+    r_token = requests.post(auth_url, data={'github_token': token})
+    if r_token.status_code == requests.codes.NOT_FOUND:
+        raise AttributeError("token problem")
+    lgr.debug('token is: {}'.format(r_token.json()['access_token']))
+    return json.loads(r_token.content)
+    r_hs = requests.get(hs_url)
+    if r_hs.status_code == requests.codes.NOT_FOUND:
+        raise AttributeError("token problem2")
+
+
+def default_props(func):
+    def inner(*args, **kwargs):
+        c = kwargs.get('c', None)
+        # if config is given, use it, if not, use default
+        c = c if c else C
+        kwargs.update({'c': c})
+        auth_token = kwargs.get('auth_token', None)
+        # if authentication token is given, use it, if not request for one
+        if not auth_token:
+            github_auth_url = c['api_url'] + c['auth_github']
+            github_handshake_url = c['api_url'] + c['auth_handshake']
+            auth_token = c['auth_token']
+            lgr = args[1]
+            auth_token = get_token(lgr, github_auth_url, github_handshake_url,
+                                   c['auth_token'])
+        kwargs.update({'auth_token': auth_token})
+        return func(*args, **kwargs)
+    return inner
+
+
+def get(url, auth_token, lgr, *args):
+    # request data
+    lgr.debug('GET >> {0}'.format(url))
+    res = requests.get(url, params=auth_token, headers=HEADERS)
+    if res.status_code == requests.codes.NOT_FOUND:
+        raise AttributeError('Request url {0} returned 404 Not Found'
+                             .format(url))
+    if not res.status_code == 200:
+        raise RuntimeError('Request url {0} failed with error: {1}:{2}'.format(
+            res.status_code, res.reason, url))
+
+    return res
+
+
+class Users:
+    @default_props
+    def __init__(self, lgr, c=None, auth_token=None, id=None):
+        self.c = c
+        self.auth_token = auth_token
+        self.id = id
+        self.lgr = lgr
+
+    def show_user(self, id):
+        if not id:
+            id = self.id
+            if not self.id:
+                raise RuntimeError('User ID must be specified')
+        url = self.c['api_url'] + self.c['users_uri'] + str(id)
+        response = get(url, self.auth_token, self.lgr)
+
+        return response['user']
+
+
+class Repositories:
+    @default_props
+    def __init__(self, lgr, c=None, auth_token=None, repo_id=None):
+        self.c = c
+        self.auth_token = auth_token
+        self.repo_id = repo_id
+        self.lgr = lgr
+
+    def show_repo(self, repo_id):
+        if not repo_id:
+            repo_id = self.repo_id
+            if not self.repo_id:
+                raise RuntimeError('repo must be specified')
+        url = self.c['api_url'] + self.c['repos_uri'] + str(repo_id)
+        response = get(url, self.auth_token, self.lgr)
+
+        return response.json()['repo']
+
+    def list_repos_with_recent_activity(self):
+        url = self.c['api_url'] + self.c['repos_uri']
+        response = get(url, self.auth_token, self.lgr)
+
+        return response.content
+
+
+class Builds:
+    @default_props
+    def __init__(self, lgr, c=None, auth_token=None, repo=None):
+        self.c = c
+        self.auth_token = auth_token
+        self.repo = repo
+        self.lgr = lgr
+
+    def list_builds(self, repo=None):
+        if not repo:
+            repo = self.repo
+            if not self.repo:
+                raise RuntimeError('Repository must be specified')
+        # define request url
+        url = \
+            self.c['api_url'] + self.c['repos_uri'] + repo + '/' + \
+            self.c['builds_uri'][0:-1]
+        response = get(url, self.auth_token, self.lgr)
+
+        return response.json()['builds'], response.json()['commits']
+
+    def show_build(self, id=None):
+        if not id:
+            raise RuntimeError('Build id must be specified')
+        url = self.c['api_url'] + self.c['builds_uri'] + str(id)
+        self.lgr.debug('Travis: requesting for build from: {}'.format(url))
+        response = get(url, self.auth_token, self.lgr)
+
+        return response.json()['build']
+
+
+class Job:
+    @default_props
+    def __init__(self, lgr, c=None, auth_token=None, job_id=None):
+        self.c = c
+        self.auth_token = auth_token
+        self.job_id = job_id
+        self.lgr = lgr
+
+    def show_job(self, job_id=None):
+        if not job_id:
+            job_id = self.job_id
+            if not self.job_id:
+                raise RuntimeError('Job ID must be specified')
+        url = self.c['api_url'] + self.c['jobs_uri'] + str(job_id)
+        response = get(url, self.auth_token, self.lgr)
+
+        return response.json()['job']
+
+
+class Log:
+    @default_props
+    def __init__(self, lgr, c=None, auth_token=None, log_id=None):
+        self.c = c
+        self.auth_token = auth_token
+        self.lgr = lgr
+        self.log_id = log_id
+
+    def show_log(self, log_id):
+        if not log_id:
+            log_id = self.log_id
+            if not self.log_id:
+                raise RuntimeError('log ID must be specified')
+        url = self.c['api_url'] + self.c['logs_uri'] + str(log_id)
+        response = get(url, self.auth_token, self.lgr)
+
+        return response.content
